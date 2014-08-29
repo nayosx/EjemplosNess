00e8cb334bd07b40083de21c4d904aea63df4e2e
NayosX Ness <nayos144@gmail.com>
2014-08-29 11:52:55 -0600

    update

--- SpinnerPersonalizado/src/com/nn/spinnerpersonalizado/MainActivity.java
+++ SpinnerPersonalizado/src/com/nn/spinnerpersonalizado/MainActivity.java
@@ -16,8 +16,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnItemSelectedListener{
 
-	private IconAdapter adaptador;
-	private Spinner spinner;
+    private IconAdapter adaptador;
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,16 +27,14 @@ public class MainActivity extends Activity implements OnItemSelectedListener{
         spinner.setAdapter(adaptador);
         spinner.setOnItemSelectedListener(this);
     }
-	@Override
-	public void onItemSelected(AdapterView<?> parent, View view, int position,
-			long id) {
-		// TODO Auto-generated method stub
-		Toast.makeText(getApplicationContext(), S.NAME_ICONS[position], Toast.LENGTH_SHORT).show();
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    // TODO Auto-generated method stub
+    	Toast.makeText(getApplicationContext(), S.NAME_ICONS[position], Toast.LENGTH_SHORT).show();
 		
-	}
-	@Override
-	public void onNothingSelected(AdapterView<?> parent) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    // TODO Auto-generated method stub	
+    }
 }
